fe5608cc22d152f392805ec7a508ff0cf2576edb
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2022-09-16 14:32:49 -0700

    2nd Commit, server started other reqs installed

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config()
 const app = express();
 
-app.listen(5000, console.log('Server is running on port 5000'));
+app.get('/', (req, res) => {
+   res.send('Api is running...');
+});
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
